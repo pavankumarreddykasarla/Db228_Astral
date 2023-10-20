4e1f47a1e6f60685c9420c247cc01b94e4be2384
Hema Venkata Sai Dumpala <52960377+HemaVenkataSaiDumpala@users.noreply.github.com>
2023-10-19 19:25:24 -0700

    Create get_item.py

---
+++ Lecture-6 In Class Assignment Code/get_item.py
@@ -0,0 +1,9 @@
+#python get_item.py (Run code using this command)
+import boto3
+
+dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+table = dynamodb.Table('Books')
+
+resp = table.get_item(Key={"Author": "John Grisham", "Title": "The Rainmaker"})
+
+print(resp['Item'])
